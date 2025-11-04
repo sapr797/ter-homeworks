678e9d911177eb6ca9a6649932db1a33f981ea60
sapr797 <unisapr797@gmail.com>
2025-11-04 11:44:24 +0300

    Update providers.tf
    Update

--- 02/src/providers.tf
+++ 02/src/providers.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "yandex" {
   # token     = var.token
-  cloud_id                 = var.cloud_id
-  folder_id                = var.folder_id
-  zone                     = var.default_zone
-  service_account_key_file = file("~/.authorized_key.json")
+  cloud_id                 = b1gsp6vsj607j08m27mr
+  folder_id                = enp09m87v5ivgh24jpdr
+  zone                     = "ru-central1-b"
+  service_account_key_file = file("~C:/distr/Distr_Terr/.authorized_key.json")
 }

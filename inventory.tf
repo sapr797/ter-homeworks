f66a43cb1c3b08d1166856a9866e776ac1c529bf
sapr797 <unisapr797@gmail.com>
2025-11-18 06:43:10 +0300

    Create inventory.tf

---
+++ inventory.tf
@@ -0,0 +1,20 @@
+# Генерация Ansible inventory файла из шаблона
+data "template_file" "inventory" {
+  template = file("${path.module}/templates/inventory.tpl")
+  
+  vars = {
+    webservers = join("\n", aws_instance.web_servers[*].public_ip)
+    # Если используете приватные IP для внутренней сети
+    # webservers = join("\n", aws_instance.web_servers[*].private_ip)
+  }
+}
+
+resource "local_file" "ansible_inventory" {
+  content  = data.template_file.inventory.rendered
+  filename = local.ansible_inventory_path
+}
+
+# Локальная переменная для пути к inventory-файлу
+locals {
+  ansible_inventory_path = "${path.module}/../ansible/hosts.ini"
+}

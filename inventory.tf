0fb178fe8883e20d462d24c8cc91a6cd0fed4c78
sapr797 <unisapr797@gmail.com>
2025-11-18 06:55:13 +0300

    Update inventory.tf

--- inventory.tf
+++ inventory.tf
@@ -1,11 +1,12 @@
+cat > inventory.tf << 'EOF'
+# inventory.tf - генерация Ansible inventory
+
 # Генерация Ansible inventory файла из шаблона
 data "template_file" "inventory" {
   template = file("${path.module}/templates/inventory.tpl")
   
   vars = {
     webservers = join("\n", aws_instance.web_servers[*].public_ip)
-    # Если используете приватные IP для внутренней сети
-    # webservers = join("\n", aws_instance.web_servers[*].private_ip)
   }
 }
 
@@ -18,3 +19,4 @@ resource "local_file" "ansible_inventory" {
 locals {
   ansible_inventory_path = "${path.module}/../ansible/hosts.ini"
 }
+EOF

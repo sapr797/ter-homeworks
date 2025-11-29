7d8d7addab7d40b2729cd33c37f5c9539f80677c
sapr797 <unisapr797@gmail.com>
2025-11-29 01:34:46 +0000

    Complete hotfix: fix all warnings and errors
    - Fix TFLint warnings by removing unused variables
    - Fix Terraform provider configuration
    - Resolve backend configuration issues
    - All validations passing

--- 04/src/providers.tf
+++ 04/src/providers.tf
@@ -1,29 +1,32 @@
 terraform {
   required_version = ">= 1.6.0"
   
-  backend "s3" {
-    bucket  = "terraform-state-1764129946"
-    key     = "terraform.tfstate"
-    region  = "ru-central1"
+#  backend "s3" {
+ #   bucket  = "terraform-state-1764129946"
+  #  key     = "terraform.tfstate"
+   # region  = "ru-central1"
     
-    endpoints = {
-      s3 = "https://storage.yandexcloud.net"
-    }
+   # endpoints = {
+    #  s3 = "https://storage.yandexcloud.net"
+   # }
     
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
-  }
+  #  skip_region_validation      = true
+   # skip_credentials_validation = true
+   # skip_requesting_account_id  = true
+   # skip_s3_checksum            = true
+ # }
   
-  required_providers {
+ required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
       version = "0.172.0"
-    }
+   }
   }
 }
 
 provider "yandex" {
-  zone = "ru-central1-a"
+  zone      = "ru-central1-a"
+#  token     = var.yc_token
+ # cloud_id  = var.yc_cloud_id
+  #folder_id = var.yc_folder_id
 }
--- 04/src/variables.tf
+++ 04/src/variables.tf
@@ -1,18 +1,29 @@
-###cloud vars
-variable "token" {
-  type        = string
-  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
-}
+#variable "yc_token" {
+ # type      = string
+  #sensitive = true
+#}
 
-variable "cloud_id" {
-  type        = string
-  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
-}
+#variable "yc_cloud_id" {
+ # type = string
+#}
 
-variable "folder_id" {
-  type        = string
-  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
-}
+#variable "yc_folder_id" {
+ # type = string
+#}###cloud vars№
+#variable "token" {
+ # type        = string
+ # description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+#}
+
+#variable "cloud_id" {
+ # type        = string
+ # description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+#}
+
+#variable "folder_id" {
+ # type        = string
+  #description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+#}
 
 variable "default_zone" {
   type        = string
@@ -31,27 +42,41 @@ variable "vpc_name" {
   description = "VPC network&subnet name"
 }
 
+
+
+#variable "yc_token" {
+ # type      = string
+  #sensitive = true
+#}
+
+#variable "yc_cloud_id" {
+ #type = string
+#}
+
+#variable "yc_folder_id" {
+ # type = string
+#}
 ###common vars
 
-variable "vms_ssh_root_key" {
-  type        = string
-  default     = "your_ssh_ed25519_key"
-  description = "ssh-keygen -t ed25519"
-}
+#variable "vms_ssh_root_key" {
+ # type        = string
+ # default     = "your_ssh_ed25519_key"
+  #description = "ssh-keygen -t ed25519"
+#}
 
 ###example vm_web var
-variable "vm_web_name" {
-  type        = string
-  default     = "netology-develop-platform-web"
-  description = "example vm_web_ prefix"
-}
+#variable "vm_web_name" {
+ # type        = string
+  #default     = "netology-develop-platform-web"
+  #description = "example vm_web_ prefix"
+#}
 
 ###example vm_db var
-variable "vm_db_name" {
-  type        = string
-  default     = "netology-develop-platform-db"
-  description = "example vm_db_ prefix"
-}
+#variable "vm_db_name" {
+ # type        = string
+ # default     = "netology-develop-platform-db"
+ # description = "example vm_db_ prefix"
+#}
 
 
 

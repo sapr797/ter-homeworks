8d3903f9d8fed6a63991be0efee32d23ed27fdd8
sapr797 <sapr797@gmail.com>
2025-12-03 01:15:14 +0000

    Homework 02: Terraform configuration for Yandex Cloud VM

---
+++ 02/src/02/demo/demostration2.tf
@@ -0,0 +1,9 @@
+# Demonstration 2
+
+terraform {
+  required_version = ">= 1.0"
+}
+
+output "demo2" {
+  value = "Demonstration 2 placeholder"
+}
---
+++ 02/src/03/demo/passwords.tf
@@ -0,0 +1,9 @@
+# Password demonstration - Empty file
+
+terraform {
+  required_version = ">= 1.0"
+}
+
+output "placeholder" {
+  value = "Password demonstration placeholder"
+}
